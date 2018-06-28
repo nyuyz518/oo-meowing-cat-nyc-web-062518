3400f3b72af7b4c75091dd8cf6c4a809c8fb1a2e
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-28 18:02:42 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,17 +1,25 @@
 ## code your solution here. 
-class Cat 
-  attr_accessor :name 
+# class Cat 
+#   attr_accessor :name 
   
-  def initialize(name)
-      @name = name  
-  end 
+#   def initialize(name)
+#       @name = name  
+#   end 
   
-  def .meow (name)
+#   def .meow (name)
+#     puts "meow!"
+#   end 
+  
+# end 
+
+# maru = Cat.new("maru")
+# maru.meow 
+
+
+class Cat
+  attr_accessor :name
+
+  def meow
     puts "meow!"
-  end 
-  
-end 
-
-maru = Cat.new("maru")
-maru.meow 
-
+  end
+end
